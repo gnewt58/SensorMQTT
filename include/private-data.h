b4690c4abad201c13867d80a107a764456c0d98d
gnewt58 <gnsellnbuy@gmail.com>
2020-05-13 13:52:54 +1000

    Create private-data.h

---
+++ include/private-data.h
@@ -0,0 +1,13 @@
+#ifndef _PRIVATE_DATA_H
+#define _PRIVATE_DATA_H
+
+#define WIFI_SSID "Your WIFI SSID"
+#define WIFI_PWD "Your WIFI Password"
+//AP definitions
+#define AP_SSID "Your AP SSID"
+#define AP_PASSWORD "Your AP Password"
+// The subnet for the AP host, third octet to follow 192.168.
+#define MQTT_OCTET3 42
+// Example will use 192.168.42.1 as MQTT host
+
+#endif // _PRIVATE_DATA_H
