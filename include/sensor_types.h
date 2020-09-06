446992548874665d2982cecc8a8f3bed80f390b9
gnewt58 <gnsellnbuy@gmail.com>
2020-09-06 16:14:00 +1000

    Add SENSORADC type

---
+++ include/sensor_types.h
@@ -0,0 +1,18 @@
+#ifndef _SENSOR_TYPES_H_
+#define _SENSOR_TYPES_H_
+
+/**
+ * Sensor type definitions. DHT11 = 11, DHT21 = 21, DHT22 = 22 as per DHT.h
+ */
+
+#ifndef DHT22
+#  include <DHT.h>
+#endif
+
+#define SENSORONEWIRE 1 // Not 11 or 21 or 22
+#define SENSORBARO 2
+#define SENSORSOIL 3
+#define SENSORVCC 4 // NEVER USE THIS!
+#define SENSORADC 5
+
+#endif // _SENSOR_TYPES_H_
