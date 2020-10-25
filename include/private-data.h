4c3cfe4765348ebefcf8f3ed8be1f4e0590ed43b
gnewt58 <gnsellnbuy@gmail.com>
2020-10-26 09:21:40 +1100

    Update private-data.h

--- include/private-data.h
+++ include/private-data.h
@@ -1,13 +1,13 @@
 #ifndef _PRIVATE_DATA_H
 #define _PRIVATE_DATA_H
 
-#define WIFI_SSID "COVID-19"
-#define WIFI_PWD "lock me down"
+#define WIFI_SSID "<nope>"
+#define WIFI_PWD "<nuh-uh>"
 //AP definitions
-#define IOT_SSID "pringlei"
-#define IOT_PASSWORD "mqtt~2015"
+#define IOT_SSID "<forgetit>"
+#define IOT_PASSWORD "<yes, but no>"
 #define MQTT_OCTET3 42
-#define MQTT_USER "ESP8266" 
-#define MQTT_PASSWORD "I am your father"
+#define MQTT_USER "<mosque>" 
+#define MQTT_PASSWORD "<eeto>"
 
 #endif // _PRIVATE_DATA_H
